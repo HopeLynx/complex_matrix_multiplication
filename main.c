4535074e14e436a021dfb0a1f7ad9880668f3c3c
HopeLynx <grigoriy.sizov@gmail.com>
2021-11-01 19:46:30 +0300

    everything works except math and file out

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+//gcc main.c -o mult
+// ./mult
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -50,10 +53,37 @@ int malloc_check(struct complex_num *arr){
 
 void create_test_files(){
     //TODO сделать создание НУЖНЫХ ФАЙЛОВ ДЛЯ ТЕСТА
+    int fd1 = open("cn1.dat",O_WRONLY|O_CREAT|O_TRUNC,0644);
+    int fd2 = open("cn2.dat",O_WRONLY|O_CREAT|O_TRUNC,0644);
+    if (fd1<0 || fd2<0) {
+        perror("create/open cn files");
+        return;
+    }
+    unsigned int r1=2,c1=3,r2=3,c2=2;
+    if(write(fd1,&r1,sizeof(unsigned int))<0)perror("write1");
+    if(write(fd1,&c1,sizeof(unsigned int))<0)perror("write1");
+    for (unsigned int j=1;j<=r1;j++){
+        for (unsigned int i=1;i<=c1*2;i++){
+            float tmp =(j*i*1.0);
+            if(write(fd1,&tmp,sizeof(float))<0)perror("write1");
+        }
+    }
+    close(fd1);
+
+    if(write(fd2,&r2,sizeof(unsigned int))<0)perror("write2");
+    if(write(fd2,&c2,sizeof(unsigned int))<0)perror("write2");
+    for (unsigned int j=1;j<=r2;j++){
+        for (unsigned int i=1;i<=c2*2;i++){
+            float tmp =(j*i*1.0);
+            if(write(fd2,&tmp,sizeof(float))<0)perror("write2");
+        }
+    }
+    close(fd2);
 }
 
 int main()
 {
+    create_test_files();
     //TODO сделать чтение файлов ДЛЯ ТЕСТА
     int fd1 = open("cn1.dat",O_RDONLY);
     int fd2 = open("cn2.dat",O_RDONLY);
@@ -63,17 +93,6 @@ int main()
         return -1;
     }
 
-    /*
-    // Создание двумерного динамического массива
-    struct complex_num** cn_matrix = (struct complex_num**)malloc(sizeof(struct complex_num*) * c1);
-        if (malloc_check(*cn_matrix)) return 1;
-
-        for(int i = 0; i < c1; i++){
-            cn_matrix[i] = (struct complex_num*)malloc(sizeof(char) * size*10);
-            if (malloc_check(str[i])) return 1;
-            scanf("%s",str[i]);
-        }
-    */
     int r1, c1, r2, c2;
     float tmp_re=0.0,tmp_im=0.0;
 
@@ -82,18 +101,13 @@ int main()
     struct complex_num *cnm1;
     cnm1 = (struct complex_num*)malloc(r1*c1 * sizeof(struct complex_num));
     if (malloc_check(cnm1)) return 1;
-    for (int i = 0; i<r1; i++)  // цикл по строкам
-    {
-        for (int j = 0; j<c1; j++)  // цикл по столбцам
-        {
-            /*
-          printf("a[%d][%d] = ", i, j);
-          scanf("%d", (a + i*m + j));
-          */
-            if (!(read(fd1,&tmp_re,sizeof(float))>0)) perror("read error");
-            if (!(read(fd1,&tmp_im,sizeof(float))>0)) perror("read error");
+    for (int i = 0; i<r1; i++){  // цикл по строкам
+        for (int j = 0; j<c1; j++){  // цикл по столбцам
+            if (read(fd1,&tmp_re,sizeof(float))<0) perror("read error");
+            if (read(fd1,&tmp_im,sizeof(float))<0) perror("read error");
             printf("%f %fi ",tmp_re,tmp_im);
-            cnm1[i*r1+j]=create_cn_params(tmp_re,tmp_im);
+            cnm1[i*c1+j]=create_cn_params(tmp_re,tmp_im);
+            //printf("\t%f %fi",cnm1[i*c1+j].re,cnm1[i*c1+j].im);
         }
         printf("\n");
     }
@@ -104,14 +118,12 @@ int main()
     struct complex_num *cnm2;
     cnm2 = (struct complex_num*)malloc(r2*c2 * sizeof(struct complex_num));
     if (malloc_check(cnm2)) return 1;
-    for (int i = 0; i<r2; i++)  // цикл по строкам
-    {
-        for (int j = 0; j<c2; j++)  // цикл по столбцам
-        {
-            if (!(read(fd2,&tmp_re,sizeof(float))>0)) perror("read error");
-            if (!(read(fd2,&tmp_im,sizeof(float))>0)) perror("read error");
-            printf("%f %fi ",tmp_re,tmp_im);
-            cnm1[i*r2+j]=create_cn_params(tmp_re,tmp_im);
+    for (int i = 0; i<r2; i++){  // цикл по строкам
+        for (int j = 0; j<c2; j++){  // цикл по столбцам
+            if (read(fd2,&tmp_re,sizeof(float))<0) perror("read error");
+            if (read(fd2,&tmp_im,sizeof(float))<0) perror("read error");
+            printf("%f,%fi ",tmp_re,tmp_im);
+            cnm2[i*c2+j]=create_cn_params(tmp_re,tmp_im);
         }
         printf("\n");
     }
@@ -123,130 +135,33 @@ int main()
         struct complex_num temp;
         ans = (struct complex_num*)malloc(r1*c2 * sizeof(struct complex_num));
         if (malloc_check(ans)) return 1;
-
-        for(int i=0;i<r1;i++)
-        {
-            for(int j=0;j<c2;j++)
-            {
+        for(int i=0;i<r1;i++){
+            for(int j=0;j<c2;j++){
                 temp=create_cn();
-                for(int k=0;k<r2;k++)
-                {
-                    add(temp,multiply(cnm1[i*r1+k],cnm2[j*r2+k]));
+                for(int k=0;k<r2;k++){
+                    struct complex_num lul = multiply(cnm1[i*c1+k],cnm2[j*r2+k]);
+                    //printf("\t%f %fi",cnm1[i*c1+k].re,cnm2[j*r2+k].im);
+                    temp = add(temp,lul);
                 }
                 ans[i*r1+j]=temp;
             }
         }
-
+        //TODO вывод в файл
         printf("\nMatrix multiplication of A*B : ");
         for(int i=0;i<r1;i++)
         {
             printf("\n\t");
             for(int j=0;j<c2;j++)
             {
-                temp = ans[i*r1+j];
+                temp = ans[i*c2+j];
                 printf("\t%f %fi",temp.re,temp.im);
             }
             printf("\n");
         }
     } else printf("\nMatrix Multiplication is not possible...!!!");
 
-
-    free(cnm1);free(cnm2);free(ans);
-
-
-
-
-
-
-    int a[r1][c1], b[r2][c2], ab[r1][c2], ba[r2][c1],i,j,k,temp;
-
-    if(c1==r2 && r1==c2)
-    {
-        /*
-        printf("\nEnter element in matrix A : ");
-        for(i=0;i<r1;i++)
-        {
-            for(j=0;j<c1;j++)
-            {
-                printf("\n Enter element : ");
-                scanf("%d",&a[i][j]);
-            }
-        }
-        printf("\nEnter element in B : ");
-        for(i=0;i<r2;i++)
-        {
-            for(j=0;j<c2;j++)
-            {
-                printf("\nEnter element : ");
-                scanf("%d",&b[i][j]);
-            }
-        }
-        */
-        for(i=0;i<r1;i++)
-        {
-            for(j=0;j<c2;j++)
-            {
-                temp=0;
-                for(k=0;k<r2;k++)
-                {
-                    temp+=a[i][k]*b[j][k];
-                }
-                ab[i][j]=temp;
-            }
-        }
-        for(i=0;i<r2;i++)
-        {
-            for(j=0;j<c1;j++)
-            {
-                temp=0;
-                for(k=0;k<r1;k++)
-                {
-                    temp+=b[i][k]*a[k][j];
-                }
-                ba[i][j]=temp;
-            }
-        }
-        printf("\nMatrix A : ");
-        for(i=0;i<r1;i++)
-        {
-            printf("\n\t");
-            for(j=0;j<c1;j++)
-            {
-                printf("%d",a[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\nMatrix B : ");
-        for(i=0;i<r2;i++)
-        {
-            printf("\n\t");
-            for(j=0;j<c2;j++)
-            {
-                printf("%d",b[i][j]);
-            }
-        }
-        printf("\nMatrix multiplication of A*B : ");
-        for(i=0;i<r1;i++)
-        {
-            printf("\n\t");
-            for(j=0;j<c2;j++)
-            {
-                printf("\t%d",ab[i][j]);
-            }
-            printf("\n");
-        }
-        printf("\nMatrix multiplication of B*A : ");
-        for(i=0;i<r2;i++)
-        {
-            printf("\n\t");
-            for(j=0;j<c1;j++)
-            {
-                printf("\t%d",ba[i][j]);
-            }
-            printf("\n");
-        }
-    }
-    else
-        printf("\nMatrix Multiplication is not possible...!!!");
+    free(cnm1);free(cnm2);
+    // TODO вывод в файл
+    free(ans);
     return 0;
 }
